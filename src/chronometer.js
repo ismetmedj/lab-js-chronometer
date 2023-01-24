746be28b4a1f2a593d59b66b38f88453a6d22490
Ismet Medjebeur <i.medjebeur@live.fr>
2023-01-24 17:55:54 +0100

    iteration 8 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,67 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      //le if suivant permet de vérifier que la fonction callback est bien une fonction, équivalent à la vérif d'entrée d'un array empty return 0
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (`${value}`.length === 1) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
     // ... your code goes here
+
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${minutes}:${seconds}`;
   }
 }
+
+// let num = 22;
+
+// console.log(`${num}`.length)
+// console.log(`${num}`)
+const chrono = new Chronometer();
+const chrono2 = new Chronometer();
+chrono.getSeconds();
